77bf325d150894f101c22351a66abebad357e1b3
nanonoam <97607867+nanonoam@users.noreply.github.com>
2024-01-25 10:19:33 +0200

    Angles + dist + kipod

---
+++ Note_With_Angle_And_Distence.py
@@ -0,0 +1,119 @@
+import numpy as np
+import cv2
+import math
+
+#constants
+camera_hight = 1
+field_of_view = (63.3,49.7)
+camera_angle = 60
+# Define lower and upper bounds for HSV color range
+hsv_low = np.array([0, 112, 199], np.uint8)
+hsv_high = np.array([100, 255, 255], np.uint8)
+
+# Define hierarchy indices
+NEXT = 0
+PREVIOUS = 1
+FIRST_CHILD = 2
+PARENT = 3
+# Define kernels for smuthing and seperating donuts
+smuthing_kernel = np.ones((5,5),np.float32)/25
+# erode_kernel = np.array([[0,1,0],[1,1,1],[0,1,0]], np.uint8)
+erode_kernel = np.ones((5, 5), np.uint8)
+
+def find_largest_contour_and_child(contours, hierarchy):
+    
+    largest_contour_index = max(range(len(contours)), key=lambda i: cv2.contourArea(contours[i]))
+    child_index = hierarchy[largest_contour_index][FIRST_CHILD]
+    biggest_child_contour_index = -1
+    biggest_child_contour_area = 0
+
+    while child_index != -1:
+        child_contour = contours[child_index]
+        child_contour_area = cv2.contourArea(child_contour)
+
+        if child_contour_area > biggest_child_contour_area:
+            biggest_child_contour_area = child_contour_area
+            biggest_child_contour_index = child_index
+
+        child_index = hierarchy[child_index][NEXT]
+    
+    return (largest_contour_index ,biggest_child_contour_index)
+
+#find x and y angles of note
+def calculat_angles(fov, x, y, h, w ,frame):
+    X_Angle = (frame.shape[0]/fov[0])*np.abs((frame.shape[0]/2) - (x+(int(w/2))))
+    Y_Angle = ((frame.shape[1]/fov[1])*np.abs((frame.shape[1]/2) - (y+(int(h/2)))) + camera_angle)
+    Angles = X_Angle, Y_Angle
+    return Angles
+def calculat_distence(Angles, hight):
+    dist = np.tan(Angles[1])*hight
+# Process video frames
+def detect_note(img):
+    #blur the imagee to smooth it
+    img = cv2.filter2D(img,-1,smuthing_kernel)
+    # Convert image to HSV color space
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # Create a mask based on the specified HSV color range
+    mask = cv2.inRange(hsv, hsv_low, hsv_high)
+    #add morphologyEx
+    mask = cv2.morphologyEx(mask, cv2.MORPH_GRADIENT, erode_kernel)
+    # Find contours in the mask
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # Draw all contours on the original image
+    cv2.drawContours(img, contours, -1, (255, 0, 255), 3)
+
+    # Process only if there are contours detected
+    if len(contours) != 0:
+        hierarchy = hierarchy[0]
+        # Find the largest contour and child contour within the largest contour
+        largest_contour_index, biggest_child_contour_index = find_largest_contour_and_child(contours, hierarchy) 
+        cv2.drawContours(img, contours, largest_contour_index, (255, 0, 0), 5)
+        # Draw the largest child contour
+        if biggest_child_contour_index != -1:
+            biggest_child_contour = contours[biggest_child_contour_index]
+            cv2.drawContours(img, [biggest_child_contour], 0, (0, 255, 0), 2)
+
+            outer_contour = contours[largest_contour_index]
+            inner_contour = contours[biggest_child_contour_index]
+
+            # Check if both outer and inner contours have areas greater than 3600
+            if (cv2.contourArea(outer_contour) > 3600) and (cv2.contourArea(inner_contour) > 3600):
+                x, y, w, h = cv2.boundingRect(outer_contour)
+                outer_aspect_ratio = float(w) / h
+                outer_center = (int(x+(w/2)), int(y+(h/2)))
+                cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+                x, y, w, h = cv2.boundingRect(inner_contour)
+                inner_aspect_ratio = float(w) / h
+                inner_center = (int(x+(w/2)), int(y+(h/2)))
+                cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+                # Check if the aspect ratios and distance between centers meet the criteria
+                if (abs(outer_aspect_ratio - inner_aspect_ratio) < 0.2) and (math.dist(outer_center, inner_center) < 8):
+                    image = cv2.putText(img, 'probably donut?☺☻♥', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2, cv2.LINE_AA)
+                    Angles = calculat_angles(field_of_view, x, y, h, w ,img)
+                    dist = calculat_distence(Angles,camera_hight)
+
+        else:
+            print("There is no child contour :(")
+
+    # Display the mask and the processed image
+    cv2.imshow('mask', mask)
+    cv2.imshow('img', img)
+
+    
+cap = cv2.VideoCapture(0)
+
+while(1):
+    
+    ret, img = cap.read()
+
+    detect_note(img)
+
+    # Check for key press to exit
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+# Release the video capture and destroy all windows
+cv2.destroyAllWindows()
