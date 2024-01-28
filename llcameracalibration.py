999c7c01936c651727b9ee2ce91d63358a1d1dd0
nanonoam <97607867+nanonoam@users.noreply.github.com>
2024-01-28 15:21:17 +0200

    llcalib

--- llcameracalibration.py
+++ llcameracalibration.py
@@ -1,5 +1,6 @@
-import numpy as np
 import cv2
+import numpy as np
+
 
 # Define the number of corners in the calibration pattern
 pattern_size = (7, 7)  # Change this according to your calibration pattern
@@ -12,17 +13,14 @@ img_points = []  # 2D points in image plane coordinates
 objp = np.zeros((pattern_size[0] * pattern_size[1], 3), np.float32)
 objp[:, :2] = np.mgrid[0:pattern_size[0], 0:pattern_size[1]].T.reshape(-1, 2)
 
-# Open video capture
-cap = cv2.VideoCapture(1)
+def runPipeline(image, llrobot):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(image, contours, -1, (255, 0, 255), 3)
+    llpython = [0,0,0,0,0,0,0,0]
 
-while True:
-    # Read frame from the camera
-    ret, frame = cap.read()
-
-    # Convert the frame to grayscale
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Find the chessboard corners
+    gray_frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
     ret, corners = cv2.findChessboardCorners(gray_frame, pattern_size, None)
 
     # If corners are found, add object points and image points
@@ -33,11 +31,10 @@ while True:
             img_points.append(corners)
 
         # Draw and display the corners
-        cv2.drawChessboardCorners(frame, pattern_size, corners, ret)
-        cv2.imshow("Calibration", frame)
+        cv2.drawChessboardCorners(image, pattern_size, corners, ret)
+        cv2.imshow("Calibration", image)
 
-    # Exit if the 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if (cv2.waitKey(1) & 0xFF == ord('q')):
         ret, camera_matrix, distortion_coeffs, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray_frame.shape[::-1], None, None)
 
         # Print the camera matrix and distortion coefficients
@@ -45,11 +42,5 @@ while True:
         print(camera_matrix)
         print("Distortion Coefficients:")
         print(distortion_coeffs)
-        break
-
-# Perform camera calibration
-
-
-# Release the video capture and close windows
-cap.release()
-cv2.destroyAllWindows()
+    
+    return contours, image, llpython
